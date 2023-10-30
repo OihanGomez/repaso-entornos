dc02233072092086857049c15ec5d4c8155a778d
ik012982i9 <mbelvermi23dw@ikzubirimanteo.com>
2023-10-30 12:44:11 +0100

    Añadir for.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola!");
+        for (int i = 0; i > 5;i++){
+
+        }
+
         }
     }
