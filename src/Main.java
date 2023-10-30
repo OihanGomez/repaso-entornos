4d1ed5dd0c66d4985322909807455b7f519bfb52
ik012982i9 <mbelvermi23dw@ikzubirimanteo.com>
2023-10-30 12:57:18 +0100

    Añadir if en el for

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i > 5;i++){
+            if (i == 4){
+                System.out.println(i);
+                i =+ 1;
+            }
 
         }
 
